6d027cb745229d578aa38a1cb1c4d47469e3d0bd
OmSwami04 <159805071+OmSwami04@users.noreply.github.com>
2026-02-10 13:12:17 +0530

    Update DelegateDemo.cs

--- Day-06/ConsoleApp1/DelegateDemo.cs
+++ Day-06/ConsoleApp1/DelegateDemo.cs
@@ -1,71 +1,137 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
     public class DelegateDemo
     {
+        //delegate int MathOperation(int a,int b);
+        // Generic Delegate
+
+        // delegate TResult GenericTwoParameterFunction<TFirst, TSecond, TResult>(TFirst a, TSecond b);
+
+        delegate void GenericTwoParameterAtion<TFirst, TSecond>(TFirst a, TSecond b);
+
         public void Run()
         {
-            // ---------- Func ----------
+            //   MathOperation op = Add;
+            // GenericTwoParameterFunction<int, int, int> genericOperation = Add;
             Func<int, int, int> genericOperation = Add;
 
-            // Multicast delegate
-            genericOperation += Sub;
-            genericOperation += Mul;
-
-            Console.WriteLine("---- Func (Multicast) ----");
-            int result = genericOperation(5, 3); // only Mul result is stored
-            Console.WriteLine($"Final result stored: {result}");
-
-            // ---------- Action ----------
             Action<string> action = PrintMessage;
             action("Hello from Action delegate!");
 
-            // ---------- Predicate ----------
             Predicate<int> predicate = IsEven;
             int testNumber = 4;
+
             Console.WriteLine($"Is {testNumber} even? {predicate(testNumber)}");
 
-            // ---------- Func with string ----------
+            return;
+
             Func<string, string, string> stringOperation = Concatenate;
-            var text = stringOperation("Hello, ", "World!");
-            Console.WriteLine(text);
+
+            var x = stringOperation("Hello, ", "World!");
+            Console.WriteLine($"Concatenation result: {x}");
+
+            //multicast deligate : adding more methodes to invocate it
+            //op += Sub;
+            //op -= Mul;
+            //op+= Div;
+            //Removing the subtract method
+            //var result = op(5, 3);
+            //Console.WriteLine($"The Result is: {result}");
+            // Multicast delegate: adding more methods to the invocation list
+            genericOperation += Sub;
+            genericOperation += Mul;
+            genericOperation += Div;
+
+            genericOperation -= Sub; // Removing the Subtract method from the invocation list
+
+            var result = genericOperation(5, 3);
+            Console.WriteLine($"Final result: {result}");
         }
 
-        // ---------- Methods ----------
-        public void PrintMessage(string message)
+        public int square(int x)
+        {
+            return x * x;
+        }
+
+        // Lambda Expression Demo
+        public void LambdaExpressionDemo()
+        {
+            Func<int, int> f;
+
+            f = (int x) => x * x;
+
+            var result = f(5);
+
+            Console.WriteLine($"result: {result}");
+        }
+
+        // Anonymous Method Demo
+        public void AnonymousMethodDemo()
+        {
+            // Using an anonymous method with a delegate
+            Func<int, int, int> operation = delegate (int a, int b)
+            {
+                Console.WriteLine($"The sum of {a} and {b} is: {a + b}");
+                return a + b;
+            };
+
+            operation(5, 3);
+        }
+
+        void PrintMessage(string message)
         {
             Console.WriteLine(message);
         }
 
-        public bool IsEven(int number)
+        bool IsEven(int number)
         {
             return number % 2 == 0;
         }
 
         public string Concatenate(string a, string b)
         {
-            return a + b;
+            string result = a + b;
+            Console.WriteLine($"Concatenating '{a}' and '{b}' results in: '{result}'");
+            return result;
         }
 
         public int Add(int a, int b)
         {
-            Console.WriteLine($"Add: {a + b}");
+            Console.WriteLine($"the val A:{a} and B:{b} and Sum is: {a + b}");
             return a + b;
         }
 
         public int Sub(int a, int b)
         {
-            Console.WriteLine($"Sub: {a - b}");
+            Console.WriteLine($"the val A:{a} and B:{b} and Sub is: {a - b}");
             return a - b;
         }
 
         public int Mul(int a, int b)
         {
-            Console.WriteLine($"Mul: {a * b}");
+            Console.WriteLine($"the val A:{a} and B:{b} and Mul is: {a * b}");
             return a * b;
         }
-    }
 
-   
+        public int Div(int a, int b)
+        {
+            if (b != 0)
+            {
+                Console.WriteLine($"the val A:{a} and B:{b} and Div is: {a / b}");
+                return a * b;
+            }
+            else
+            {
+                Console.WriteLine("the value is Invalid ");
+                return 0;
+            }
+        }
+    }
 }
