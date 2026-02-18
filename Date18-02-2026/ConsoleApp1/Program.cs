91f9c67c351eca2590181b83c247a0c7a549d917
Om swami <omswami967@gmail.com>
2026-02-18 15:46:09 +0530

    date18-02

---
+++ Date18-02-2026/ConsoleApp1/Program.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Reflection.Emit;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var _context = new CrmDbContext();
+
+            var customers = _context.Customers.ToList();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"Customer ID: {customer.CustomerId}, Name: {customer.Name}, Email: {customer.Email}");
+            }
+        }
+    }
+
+    class CrmDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString =
+            "server=localhost;port=3306;database=mig;user=root;password=Swamiom11@;";
+
+            optionsBuilder.UseMySql(
+                connectionString,
+                ServerVersion.AutoDetect(connectionString)
+            );
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CustomerType>()
+                .HasData(
+                    new CustomerType { Id = 1, TypeName = "Regular" },
+                    new CustomerType { Id = 2, TypeName = "Premium" }
+                );
+
+            modelBuilder.Entity<Order>()
+                .HasKey(o => o.OrderId);
+
+            modelBuilder.Entity<Order>()
+                .HasOne(o => o.Customer)
+                .WithMany()
+                .HasForeignKey(o => o.CustomerId);
+        }
+    }
+
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+
+    public class CustomerType
+    {
+        public int Id { get; set; }
+        public string? TypeName { get; set; }
+    }
+
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        [ForeignKey("Customer")]
+        public int CustomerId { get; set; }
+        public Customer? Customer { get; set; }
+    }
+}
